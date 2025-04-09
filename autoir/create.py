9ffc4f5883af2f1272cc8840a8f0032babc0bd36
Daniel Schwalbe-Koda <danielskoda@gmail.com>
2025-04-09 08:47:16 -0700

    add kwargs and box

--- autoir/create.py
+++ autoir/create.py
@@ -16,7 +16,7 @@ DEFAULT_BOX_SIZE_LIQ = 2.6  # nm for liq phase
 DEFAULT_NUM_MOLS = 100
 
 
-def estimate_box_size(smiles: str, n_mols: int = DEFAULT_NUM_MOLS, scaling: float = 2):
+def estimate_box_size(smiles: str, n_mols: int = DEFAULT_NUM_MOLS, scaling: float = 0.01):
     mol = Chem.AddHs(Chem.MolFromSmiles(smiles))
     Chem.EmbedMolecule(mol)
     vol = Chem.ComputeMolVolume(mol)
@@ -76,11 +76,11 @@ def gas_simulation(smiles, box_size: float = DEFAULT_BOX_SIZE_GAS):
     return mol, topology
 
 
-def liq_simulation(smiles, n_mols: int = DEFAULT_NUM_MOLS, box_size: float = None):
+def liq_simulation(smiles, n_mols: int = DEFAULT_NUM_MOLS, box_size: float = None, **kwargs):
     mol = Molecule.from_smiles(smiles, allow_undefined_stereo=True)
 
     if box_size is None:
-        box_size = estimate_box_size(smiles, n_mols=n_mols)
+        box_size = estimate_box_size(smiles, n_mols=n_mols, **kwargs)
     else:
         box_size = float(box_size)
 
@@ -92,21 +92,21 @@ def liq_simulation(smiles, n_mols: int = DEFAULT_NUM_MOLS, box_size: float = Non
     interchange.to_lammps("out.lmp")
     mdconfig = MDConfig.from_interchange(interchange)
     mdconfig.write_lammps_input(input_file="header.in", interchange=interchange)
-    return mol, topology
+    return topology, interchange
 
 
 def mix_simulation(
-    smiles1, smiles2, ratio: float = 0.5, n_mols: int = DEFAULT_NUM_MOLS
+    smiles1, smiles2, ratio: float = 0.5, n_mols: int = DEFAULT_NUM_MOLS, **kwargs
 ):
     mol1 = Molecule.from_smiles(smiles1, allow_undefined_stereo=True)
     mol2 = Molecule.from_smiles(smiles2, allow_undefined_stereo=True)
 
     # estimates the size of the box given the number of heavy atoms
     box_size = (
-        estimate_box_size(smiles1, n_mols=n_mols) + estimate_box_size(smiles2, n_mols=n_mols)
+        estimate_box_size(smiles1, n_mols=n_mols, **kwargs) + estimate_box_size(smiles2, n_mols=n_mols, **kwargs)
     ) / 2
 
-    topology = topology_binary(mol1, mol2, ratio, box_size=box_size)
+    topology = topology_binary(mol1, mol2, ratio, n_mols=n_mols, box_size=box_size)
     ff = ForceField(DEFAULT_FF)
     interchange: Interchange = Interchange.from_smirnoff(
         force_field=ff, topology=topology
@@ -114,4 +114,4 @@ def mix_simulation(
     interchange.to_lammps("out.lmp")
     mdconfig = MDConfig.from_interchange(interchange)
     mdconfig.write_lammps_input(input_file="header.in", interchange=interchange)
-    return topology
+    return topology, interchange
