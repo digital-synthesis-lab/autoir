7df7a9294a5fe56c3f4ac1978e982b501d9fff79
Daniel Schwalbe-Koda <danielskoda@gmail.com>
2025-04-10 09:24:44 -0700

    truncate autocorr

--- autoir/cli/analyze.py
+++ autoir/cli/analyze.py
@@ -1,12 +1,11 @@
 import click
-import uuid
-import os
-from autoir.analyze import process_file
+from autoir.analyze import process_file, DEFAULT_TRUNCATE
 
 
 @click.command("analyze")
 @click.option("-i", "--input", default="dipoles.csv", help="Input file")
 @click.option("-o", "--output", default="ir.csv", help="Output file")
-def analyze_file(input, output):
+@click.option("-t", "--truncate_autocorr", default=DEFAULT_TRUNCATE, help="Number of steps to truncate the autocorrelation function")
+def analyze_file(input, output, truncate_autocorr):
     """Compute the IR spectrum from the output LAMMPS simulation"""
-    process_file(input, output)
+    process_file(input, output, truncate_autocorr=truncate_autocorr)
