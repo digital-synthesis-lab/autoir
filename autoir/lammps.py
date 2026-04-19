0b77eb7bd3a34afd7a063b145299ba5b5ed53454
Daniel Schwalbe-Koda <danielskoda@gmail.com>
2026-04-18 18:40:57 -0700

    delete old code

--- autoir/lammps.py
+++
@@ -1,9 +0,0 @@
-from openff.interchange import Interchange
-from openff.interchange.components.mdconfig import MDConfig
-
-
-def write_lammps_file(interchange: Interchange):
-    interchange.to_lammps("out.lmp")
-    mdconfig = MDConfig.from_interchange(interchange)
-    mdconfig.write_lammps_input(input_file="header.in", interchange=interchange)
-    return mdconfig
